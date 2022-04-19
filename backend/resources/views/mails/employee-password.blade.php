5a995a847e42446945e9b2a7cc0ec0396a547d54
Mitesh Kadivar <mitesh.kadivar@bytestechnolab.in>
2022-04-19 15:40:15 +0530

    Added login url in email template.

--- backend/resources/views/mails/employee-password.blade.php
+++ backend/resources/views/mails/employee-password.blade.php
@@ -1,7 +1,8 @@
 <html>
     <body>
         Hello <b>{!!$name!!}</b>, <br />
-        Here, below is your password for login into Sample Reservation Platform:
+        Here, below is your password for login into Sample Reservation Platform: <br />
+        Login Url : <a href="http://localhost:4200/auth">http://localhost:4200/auth</a>
         {!!$html!!}
 
         Regards, <br />
